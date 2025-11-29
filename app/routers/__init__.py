ae3d51a7b2686d7ed016eaeb7ac349d84b82db4f
Andres Fernando Basto Bejarano <afbasto49@ucatolica.edu.co>
2025-11-28 19:49:20 -0500

    errores corregidos

---
+++ app/routers/__init__.py
@@ -0,0 +1,6 @@
+from .npcs import router as npcs
+from .items import router as items
+from .misiones import router as misiones
+from .ubicaciones import router as ubicaciones
+from .busqueda import router as busqueda
+from .reportes import router as reportes
