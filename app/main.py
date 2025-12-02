a22ca8196886c99083fa449c7bfa5b9cd0e3349a
Andres Fernando Basto Bejarano <afbasto49@ucatolica.edu.co>
2025-12-01 19:02:23 -0500

    pruebas front

--- app/main.py
+++ app/main.py
@@ -1,14 +1,23 @@
-from fastapi import FastAPI
 from app.db import init_db
 from app.routers import npcs, items, misiones, ubicaciones, busqueda, reportes
 from dotenv import load_dotenv
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 load_dotenv()
 app = FastAPI(title="Proyecto NPC")
 
 @app.on_event("startup")
 def on_startup():
     init_db()
+templates = Jinja2Templates(directory="app/templates")
 
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
 app.include_router(npcs, prefix="/npcs", tags=["NPCs"])
 app.include_router(items, prefix="/items", tags=["Items"])
 app.include_router(misiones, prefix="/misiones", tags=["Misiones"])
