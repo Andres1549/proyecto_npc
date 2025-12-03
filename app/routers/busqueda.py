2f309fdf37dde54a1a8033233e69d97e1c6606e5
Andres Fernando Basto Bejarano <afbasto49@ucatolica.edu.co>
2025-12-02 22:44:24 -0500

    version 5 listas

--- app/routers/busqueda.py
+++ app/routers/busqueda.py
@@ -3,6 +3,10 @@ from sqlmodel import select, Session
 from app.db import get_session
 from app.models import NPC, Item, Mision, Ubicacion
 import unicodedata
+from fastapi import Request
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="app/templates")
 
 router = APIRouter(tags=["Búsqueda Global"])
 
@@ -13,9 +17,11 @@ def normalizar(texto: str) -> str:
     texto = "".join(c for c in texto if unicodedata.category(c) != "Mn")
     return texto.lower()
 
+
 @router.get("/buscar")
-def busqueda_global(
-    termino: str = Query(..., description="Término a buscar"),
+def busqueda_html(
+    request: Request,
+    termino: str = Query(...),
     session: Session = Depends(get_session)
 ):
     termino_norm = normalizar(termino)
@@ -37,12 +43,14 @@ def busqueda_global(
         if termino_norm in normalizar(u.nombre) or termino_norm in normalizar(u.descripcion)
     ]
 
-    return {
-        "termino": termino,
-        "resultados": {
+    return templates.TemplateResponse(
+        "listas/busqueda.html",
+        {
+            "request": request,
+            "termino": termino,
             "npcs": npcs,
             "items": items,
             "misiones": misiones,
             "ubicaciones": ubicaciones
         }
-    }
+    )
